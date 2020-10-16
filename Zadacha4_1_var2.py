4d96d11a6e98f44f3b3f2e1bef1fb8bcd1767b86
Andrey <perimeyson@yandex.ru>
2020-10-16 19:52:41 +0300

    Второй вариант со срезом

--- Zadacha4_1_var2.py
+++ Zadacha4_1_var2.py
@@ -1,11 +1,9 @@
-from sys import argv
-
-time, salary, bonus = argv[1:]
+vremy, salary, bonus = argv[1:]
 try:
-    time = int(time)
-    salary = int(salary)
+    vremy = int(vremy)
+    zarplata = int(zarplata)
     bonus = int(bonus)
-    res = time * salary + bonus
+    res = vremy * zarplata + bonus
     print(f'заработная плата сотрудника  {res}')
 except ValueError:
     print('Not a number')
