0dd1573c606f1167549f624f299846a79b69ae34
AndreyB67 <72445378+AndreyB67@users.noreply.github.com>
2021-05-11 18:12:57 +0300

    Update task8_1.py

--- task8_1.py
+++ task8_1.py
@@ -1,11 +1,11 @@
 import hashlib
 
-def substr_count(string):
+def substr_count(str):
     sum_substring = set()
 
-    for i in range(len(string)):
-        for j in range(len(string), i,-1):
-            hash_str = hashlib.sha1(string[i:j].encode('utf-8')).hexdigest()
+    for i in range(len(str)):
+        for j in range(len(str), i,-1):
+            hash_str = hashlib.sha1(str[i:j].encode('utf-8')).hexdigest()
             sum_substring.add(hash_str)
     return len(sum_substring)-1
 
