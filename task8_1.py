b99f077a92157388ecef55915cd5b516b601252e
AndreyB67 <72445378+AndreyB67@users.noreply.github.com>
2021-05-11 18:14:21 +0300

    Update task8_1.py

--- task8_1.py
+++ task8_1.py
@@ -6,8 +6,8 @@ def substr_count(str):
     for i in range(len(str)):
         for j in range(len(str), i,-1):
             hash_str = hashlib.sha1(str[i:j].encode('utf-8')).hexdigest()
-            sum_substring.add(hash_str)
-    return len(sum_substring)-1
+            sum_substr.add(hash_str)
+    return len(sum_substr)-1
 
 s = input("Введите строку: ")
 print(substr_count(s))
